96aa1827a6a8ab2b4698be007facf614e3a6646a
bbtheo <tblauberg@gmail.com>
2022-10-25 13:37:39 +0300

    fucked up

---
+++ index.knit.md
@@ -0,0 +1,16 @@
+---
+documentclass: article
+bibliography: bibliography.bib
+biblio-style: "apalike"
+fontsize: 12pt
+papersize: a4
+linestretch: 1.5
+link-citations: yes
+---
+
+\newpage
+
+# Introduction {#intro}
+
+
+
--- render.R
+++ render.R
@@ -1,8 +1,11 @@
 #knit a pdf document
 bookdown::render_book('index.Rmd', 'bookdown::pdf_document2')
 
+
 #knit a pdf document
 bookdown::render_book('index.Rmd', 'bookdown::pdf_book')
 
 #knit a html document
 bookdown::render_book('index.Rmd', 'bookdown::html_document2')
+
+source()
